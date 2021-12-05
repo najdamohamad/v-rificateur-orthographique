529c5efeb2525b3ffdcdd984a984855459b06bce
Leo JOLY <leo.joly2@grenoble-inp.org>
2021-12-06 00:10:05 +0100

    comment

--- include/arbre_radix.h
+++ include/arbre_radix.h
@@ -9,14 +9,18 @@
 #include "arbre_prefix.h"
 #include "hash.h"
 
+//Transforme un arbre prefix en radix :o
 void transform_prefix_into_radix(arbre* a);
+//Compression 2 : trouve les répétitions dans l'arbre et tente de les réduires
 arbre partage_suffix(arbre a, table_hachage* alr_meet, bool* reloc);
 
+//Supprime l'arbre en ignorant les noeuds dédoublés (marquage) et les ajoute dans une liste ou table de hash
 void detruire_arbre_radix(arbre a, table_hachage* duplic);
-void radix_delete(void* e);
 
-int suffix_compare(void* e1, void* e2);
-void chuuuuuu(void*);
-int hash_suffix(void* e, unsigned c);
+//Fonctions annexes pour structures
+void radix_delete(void* e);             //Suprime un noeud radix
+int suffix_compare(void* e1, void* e2); //Compare deux arbres
+void chuuuuuu(void*);                   //Fonction vide
+int hash_suffix(void* e, unsigned c);   //Calcule le hash de la valeur du noeud
 
 #endif
